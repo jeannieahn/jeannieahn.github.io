07b4b6d207626984aec6ec39f372f49176831ced
jeannieahn <jeannieahn@yahoo.com>
2016-04-26 23:15:25 -0700

    upload 2

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$("#reminder").click(function() {
+		if ($('.popup').hasClass('popup-active')) {
+			$('.popup').removeClass('popup-active');
+			setTimeout(function(){
+				$('.popup').hide();
+			}, 600);
+		} else {
+		$('.popup').show();
+		setTimeout (function(){
+			$(".popup").addClass('popup-active');
+		}, 0);
+	}
+	});
+});
